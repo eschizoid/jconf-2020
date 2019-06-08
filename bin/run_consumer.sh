6afe369773c1010e97f2c28af5de0799518b3e01
Grace Chang <grcechng@gmail.com>
2019-06-07 22:42:44 -0500

    small cleanup on naming and details in readme

--- bin/run_consumer.sh
+++ bin/run_consumer.sh
@@ -6,9 +6,9 @@ set +a
 export PYTHONPATH=$SPARK_HOME/python/:$SPARK_HOME/python/lib/py4j-0.10.7-src.zip
 
 if lsof -Pi :9009 -sTCP:LISTEN -t >/dev/null ; then
-    python3 ./streaming/src/main/python/ChicagoCloudConference/spark_cosumer.py
+    python3 ./streaming/src/main/python/ChicagoCloudConference/spark_consumer.py
 else
-    echo "Socket not open. Start consumer first!"
+    echo "Socket not open. Start producer first!"
     exit 1
 fi
 
