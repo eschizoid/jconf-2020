01a8e01d6a0958b60fcca0eeb76ca7c6f3b63944
Mariano Gonzalez <mariano.gonzalez@uptake.com>
2019-06-21 12:45:09 -0500

    Added setup.py file

--- bin/run_transformer.sh
+++ bin/run_transformer.sh
@@ -3,4 +3,9 @@ set -a
 source .env
 set +a
 
-java -jar ./transformation/build/libs/transformation-1.0-SNAPSHOT-all.jar
+${SPARK_HOME}/bin/spark-submit \
+    --master "${SPARK_MASTER}" \
+    --deploy-mode "${SPARK_DEPLOY_MODE}" \
+    --conf "spark.executor.instances=1" \
+    --conf "spark.kubernetes.container.image=docker.io/eschizoid/spark:2.4.3" \
+    transformation/build/libs/transformation-1.0-SNAPSHOT-all.jar
--- transformation/src/test/scala/ChicagoCloudConference/TransformerSpec.scala
+++ transformation/src/test/scala/ChicagoCloudConference/TransformerSpec.scala
@@ -1,28 +1,61 @@
 package ChicagoCloudConference
 
-import org.apache.spark.sql.functions.get_json_object
+import java.time.LocalDate
+import java.util.regex.Pattern
+
+import org.apache.spark.sql.functions.{coalesce, get_json_object, udf}
+import org.apache.spark.sql.streaming.OutputMode
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
 import org.scalatest.{FlatSpec, Matchers}
+
+import scala.collection.mutable.ListBuffer
 
 class TransformerSpec extends FlatSpec with Matchers with SparkSupport {
 
   import sqlContext.implicits._
 
-  "Transformer should" should "yield the columns created_at, text, and location" in {
+  "Transformer should" should "yield the columns created_at, text, location, and hashtags" in {
+
+    val pattern = Pattern.compile("#(\\w*[0-9a-zA-Z]+\\w*[0-9a-zA-Z])")
+
+    def regexp_extractAll =
+      udf((text: String) => {
+        val matcher = pattern.matcher(text)
+        val result  = ListBuffer.empty[String]
+        while (matcher.find()) {
+          result += matcher.group()
+        }
+        result.mkString(",")
+      })
+
     val schema = StructType(Array(StructField("value", StringType, nullable = true)))
-    val parquet = spark.read
+    val parquet = spark.readStream
       .schema(schema)
-      .json(s"s3a://chicago-cloud-conference-2019/bronze/*/*/part-*.json")
+      .format("json")
+      .load(s"s3a://chicago-cloud-conference-2019/bronze/*/*/part-*.json")
       .select(
         get_json_object($"value", "$.created_at").alias("created_at"),
+        get_json_object($"value", "$.extended_tweet").alias("extended_tweet"),
         get_json_object($"value", "$.text").alias("text"),
         get_json_object($"value", "$.user").alias("user")
       )
-      .toDF
-      .select($"created_at", $"text", $"user")
+      .select($"created_at", $"extended_tweet", $"text", $"user")
       .withColumn("location", get_json_object($"user", "$.location"))
-      .toDF
+      .withColumn("full_text", get_json_object($"extended_tweet", "$.full_text"))
+      .select($"created_at", $"full_text", $"text", $"location")
+      .withColumn("text", coalesce($"full_text", $"text"))
+      .drop("full_text")
       .select($"created_at", $"text", $"location")
-    parquet.foreach(x => println(x))
+      .withColumn("hashtags", regexp_extractAll($"text"))
+      .na
+      .fill("", Seq("text", "location", "hashtags"))
+
+    val query = parquet.writeStream
+      .option("checkpointLocation", "checkpoint_transformation_chicago-cloud-conference")
+      .option("path", s"s3a://chicago-cloud-conference-2019/silver/${LocalDate.now.toString}")
+      .outputMode(OutputMode.Append)
+      .start()
+
+    query.awaitTermination(10000)
   }
 }
