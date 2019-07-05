4a527b70df0f8ac1f6d4c57399ec6e533e3c8ba7
Mariano Gonzalez <mariano.gonzalez@uptake.com>
2019-07-05 09:05:42 -0500

    Removed k8s secrets in favor of env variables

--- bin/build_docker_images.sh
+++ bin/build_docker_images.sh
@@ -3,15 +3,19 @@
 cd infrastructure/spark
 
 echo "Building spark 2.4.3 image"
-docker build -t spark:2.4.3 -f Dockerfile .
+docker build --no-cache \
+    -t spark:2.4.3 -f Dockerfile .
 
 echo "Building spark python image"
-docker build -t spark:python -f Dockerfile.python .
+docker build --no-cache \
+    -t spark:python -f Dockerfile.python .
 
 echo "Building spark R image"
-docker build -t spark:R -f Dockerfile.R .
+docker build --no-cache \
+    -t spark:R -f Dockerfile.R .
 
 cd ../zeppelin
 
-echo "Building zeppelin image"
-docker build -t zeppelin:0.9.0-SNAPSHOT .
+#echo "Building zeppelin image"
+#docker build --no-cache \
+#    -t zeppelin:0.9.0-SNAPSHOT .
--- bin/run_aggregator.sh
+++ bin/run_aggregator.sh
@@ -5,30 +5,17 @@ set +a
 
 ${SPARK_SUBMIT_BIN}/bin/spark-submit \
     --master "${SPARK_MASTER}" \
-    --deploy-mode "${SPARK_DEPLOY_MODE}" \
-    --conf "spark.executor.cores=1" \
-    --conf "spark.executor.instances=${SPARK_EXECUTOR_INSTANCES}" \
-    --conf "spark.executor.memory=${SPARK_EXECUTOR_MEMORY}" \
+    --deploy-mode ${SPARK_DEPLOY_MODE} \
+    --conf "spark.kubernetes.authenticate.driver.serviceAccountName=spark" \
     --conf "spark.kubernetes.container.image=docker.io/eschizoid/spark:R" \
-    --conf "spark.kubernetes.authenticate.driver.serviceAccountName=default" \
-    --conf "spark.kubernetes.driver.secrets.chicago-cloud-conference-secrets=/etc/secrets" \
-    --conf "spark.kubernetes.driver.secretKeyRef.AWS_ACCESS_KEY_ID=chicago-cloud-conference-secrets:aws_access_key_id" \
-    --conf "spark.kubernetes.driver.secretKeyRef.AWS_SECRET_ACCESS_KEY=chicago-cloud-conference-secrets:aws_secret_access_key" \
-    --conf "spark.kubernetes.driver.secretKeyRef.BUCKET_NAME=chicago-cloud-conference-secrets:bucket_name" \
-    --conf "spark.kubernetes.driver.secretKeyRef.CONSUMER_KEY=chicago-cloud-conference-secrets:consumer_key" \
-    --conf "spark.kubernetes.driver.secretKeyRef.CONSUMER_SECRET=chicago-cloud-conference-secrets:consumer_secret" \
-    --conf "spark.kubernetes.driver.secretKeyRef.SPARK_DEPLOY_MODE=chicago-cloud-conference-secrets:spark_deploy_mode" \
-    --conf "spark.kubernetes.driver.secretKeyRef.SPARK_MASTER=chicago-cloud-conference-secrets:spark_master" \
-    --conf "spark.kubernetes.driver.secretKeyRef.TCP_IP=chicago-cloud-conference-secrets:tcp_ip" \
-    --conf "spark.kubernetes.driver.secretKeyRef.TCP_PORT=chicago-cloud-conference-secrets:tcp_port" \
-    --conf "spark.kubernetes.executor.secrets.chicago-cloud-conference-secrets=/etc/secrets" \
-    --conf "spark.kubernetes.executor.secretKeyRef.AWS_ACCESS_KEY_ID=chicago-cloud-conference-secrets:aws_access_key_id" \
-    --conf "spark.kubernetes.executor.secretKeyRef.AWS_SECRET_ACCESS_KEY=chicago-cloud-conference-secrets:aws_secret_access_key" \
-    --conf "spark.kubernetes.executor.secretKeyRef.BUCKET_NAME=chicago-cloud-conference-secrets:bucket_name" \
-    --conf "spark.kubernetes.executor.secretKeyRef.CONSUMER_KEY=chicago-cloud-conference-secrets:consumer_key" \
-    --conf "spark.kubernetes.executor.secretKeyRef.CONSUMER_SECRET=chicago-cloud-conference-secrets:consumer_secret" \
-    --conf "spark.kubernetes.executor.secretKeyRef.SPARK_DEPLOY_MODE=chicago-cloud-conference-secrets:spark_deploy_mode" \
-    --conf "spark.kubernetes.executor.secretKeyRef.SPARK_MASTER=chicago-cloud-conference-secrets:spark_master" \
-    --conf "spark.kubernetes.executor.secretKeyRef.TCP_IP=chicago-cloud-conference-secrets:tcp_ip" \
-    --conf "spark.kubernetes.executor.secretKeyRef.TCP_PORT=chicago-cloud-conference-secrets:tcp_port" \
-    local:///${SPARK_HOME}/examples/aggregation/aggregator.R
+    --conf "spark.kubernetes.driverEnv.AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" \
+    --conf "spark.kubernetes.driverEnv.AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" \
+    --conf "spark.kubernetes.driverEnv.BUCKET_NAME=${BUCKET_NAME}" \
+    --conf "spark.kubernetes.driverEnv.CONSUMER_KEY=${CONSUMER_KEY}" \
+    --conf "spark.kubernetes.driverEnv.CONSUMER_SECRET=${CONSUMER_SECRET}" \
+    --conf "spark.kubernetes.driverEnv.JAVA_HOME=/usr/lib/jvm/java-1.8-openjdk" \
+    --conf "spark.kubernetes.driverEnv.LOGGING_LEVEL=${LOGGING_LEVEL}" \
+    --conf "spark.kubernetes.driverEnv.SPARK_MASTER=${SPARK_MASTER}" \
+    --conf "spark.kubernetes.driverEnv.TCP_IP=${TCP_IP}" \
+    --conf "spark.kubernetes.driverEnv.TCP_PORT=${TCP_PORT}" \
+    local:///opt/spark/examples/aggregation//aggregator.R
