4b34acf15b5478f665293133074850ef9f94d9fb
Mariano Gonzalez <eschizoid@hotmail.com>
2019-07-06 08:35:17 -0500

    Fixed pythond dependecies

--- bin/zip_python_dependencies.sh
+++ bin/zip_python_dependencies.sh
@@ -2,8 +2,15 @@
 
 cd streaming
 
+cat << 'EOF' > ~/.pydistutils.cfg
+[install]
+prefix=
+EOF
+
 pip3 install \
     -t dependencies \
     -r pinned.txt
 
 zip -r dependencies.zip dependencies
+
+rm -rf ~/.pydistutils.cfg
